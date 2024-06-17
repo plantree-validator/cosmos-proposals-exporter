99f33fb9da98c0929fe1bbf7eb696a5abdb2d22e
alit-by <litasov.o@gmail.com>
2024-06-17 12:27:36 +0300

    Multiple_changes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Cosmos Chains Proposals Prometheus Exporter
 
-This Prometheus exporter collects metrics about Cosmos chains proposals, focusing on active proposals that a specific validator has not voted on. The exporter can be configured to monitor multiple Cosmos chains.
+This Prometheus exporter collects metrics about Cosmos chains proposals, focusing on active proposals that a specific chain address has not voted on. The exporter can be configured to monitor multiple Cosmos chains.
 
 ## Configuration
 
@@ -10,13 +10,13 @@ Create a `config.yaml` file to configure the exporter. Below is an example confi
 chains:
   - name: likecoin
     node_url: https://mainnet-node.like.co
-    validator_address: likevaloper********
+    validator_address: like********
+    alias: likecoin_validator
   - name: osmosis
     node_url: https://lcd.osmosis.zone
-    validator_address: osmovaloper******
+    validator_address: osmo******
+    alias: osmosis_validator
 # Scrape interval in seconds
 scrape_interval: 600  
 # Count of last proposals to check status 
 scrape_last_proposals_count: 50
-# Count of proposal voters to be searched in
-voters_query_count: 200
--- exporter.py
+++ exporter.py
@@ -1,6 +1,6 @@
 import yaml
 import requests
-from prometheus_client import start_http_server, Gauge, Info, REGISTRY
+from prometheus_client import start_http_server, Gauge, Info, REGISTRY, PROCESS_COLLECTOR, PLATFORM_COLLECTOR, GC_COLLECTOR
 import time
 import logging
 from datetime import datetime, timezone
@@ -17,15 +17,18 @@ with open('config.yaml') as config_file:
 CHAINS = config.get('chains', [])
 SCRAPE_INTERVAL = config.get('scrape_interval', 60)
 SCRAPE_PROPOSALS_COUNT = config.get('scrape_last_proposals_count', 50)
-VOTERS_QUERY_COUNT = config.get('voters_query_count', 200)
 
 
 PROPOSALS_ENDPOINT = "/cosmos/gov/v1/proposals?pagination.reverse=true&pagination.limit={scrape_proposals_count}&pagination.count_total=true"
-VOTES_ENDPOINT = "/cosmos/gov/v1beta1/proposals/{proposal_id}/votes?pagination.limit={voters_query_count}"
+VOTES_ENDPOINT = "/cosmos/gov/v1beta1/proposals/{proposal_id}/votes/{chain_address}"
+
+# Retry settings
+MAX_RETRIES = 5
+RETRY_WAIT_TIME = 1  # in seconds
 
 # Prometheus metrics
-not_voted_gauge = Gauge('validator_not_voted_on_proposals_count', 'Number of active proposals the validator has not voted on', ['chain'])
-not_voted_info = Info('validator_not_voted_on_proposal', 'Details of active proposals the validator has not voted on', ['chain', 'proposal_id'])
+not_voted_gauge = Gauge('address_not_voted_on_proposals_count', 'Number of active proposals the address has not voted on', ['chain', 'alias'])
+not_voted_info = Info('address_not_voted_on_proposal', 'Details of active proposals the address has not voted on', ['chain', 'alias', 'chain_address', 'proposal_id'])
 
 def fetch_proposals(node_url):
     try:
@@ -40,18 +43,38 @@ def fetch_proposals(node_url):
         logger.error(f"Error fetching proposals: {e}")
         return []
 
-def fetch_votes(node_url, proposal_id):
-    try:
-        url = node_url + VOTES_ENDPOINT.format(proposal_id=proposal_id,voters_query_count=VOTERS_QUERY_COUNT)
-        logger.info(f"Fetching votes from {url}")
-        response = requests.get(url)
-        response.raise_for_status()
-        votes = response.json().get('votes', [])
-        logger.info(f"Fetched {len(votes)} votes for proposal {proposal_id}")
-        return votes
-    except requests.RequestException as e:
-        logger.error(f"Error fetching votes: {e}")
-        return []
+def fetch_vote(node_url, proposal_id, chain_address):
+    retries = 0
+    while retries < MAX_RETRIES:
+        try:
+            url = node_url + VOTES_ENDPOINT.format(proposal_id=proposal_id, chain_address=chain_address)
+            logger.info(f"Fetching vote from {url}")
+            response = requests.get(url)
+            response.raise_for_status()
+            vote_response = response.json()
+            
+            if 'vote' in vote_response:
+                vote_option = vote_response['vote']['options'][0]['option']
+                logger.info(f"Vote found for proposal {proposal_id}. Voter: {chain_address}, Option: {vote_option}")
+            return vote_response
+        except requests.HTTPError as e:
+            if response.status_code == 429:
+                logger.warning(f"Rate limit exceeded. Retrying in {RETRY_WAIT_TIME} seconds...")
+                time.sleep(RETRY_WAIT_TIME)
+                retries += 1
+                continue
+            elif response.status_code == 400:
+                vote_response = response.json()
+                if vote_response.get('code') == 3:
+                    logger.info(f"No vote found for proposal {proposal_id} by address {chain_address} (code 3)")
+                    return None
+            logger.error(f"Error fetching vote: {e}")
+            return None
+        except requests.RequestException as e:
+            logger.error(f"Error fetching vote: {e}")
+            return None
+    logger.error(f"Max retries reached. Could not fetch vote for proposal {proposal_id}")
+    return None
 
 def is_proposal_active(proposal):
     now = datetime.now(timezone.utc)
@@ -61,8 +84,9 @@ def is_proposal_active(proposal):
 
 def check_not_voted_proposals(chain):
     node_url = chain['node_url']
-    validator_address = chain['validator_address']
+    chain_address = chain['chain_address']
     chain_name = chain['name']
+    chain_alias = chain['alias']
     
     proposals = fetch_proposals(node_url)
     not_voted_proposals = []
@@ -72,23 +96,24 @@ def check_not_voted_proposals(chain):
             continue
         proposal_id = proposal['id']
         proposal_title = proposal['title']
-        votes = fetch_votes(node_url, proposal_id)
-        if not any(vote['voter'] == validator_address for vote in votes):
+        vote = fetch_vote(node_url, proposal_id, chain_address)
+        if vote is None or vote.get('code') == 3:
             not_voted_proposals.append({
                 'id': proposal_id,
                 'title': proposal_title
             })
 
-    not_voted_gauge.labels(chain=chain_name).set(len(not_voted_proposals))
+    not_voted_gauge.labels(chain=chain_name, alias=chain_alias).set(len(not_voted_proposals))
     for proposal in not_voted_proposals:
-        not_voted_info.labels(chain=chain_name, proposal_id=proposal['id']).info({
+        not_voted_info.labels(chain=chain_name, alias=chain_alias, chain_address=chain_address, proposal_id=proposal['id']).info({
             'proposal_title': proposal['title']
         })
 
 def main():
     # Disable default Prometheus metrics
-    for collector in list(REGISTRY._collector_to_names.keys()):
-        REGISTRY.unregister(collector)
+    REGISTRY.unregister(PROCESS_COLLECTOR)
+    REGISTRY.unregister(PLATFORM_COLLECTOR)
+    REGISTRY.unregister(GC_COLLECTOR)
 
     start_http_server(8000)
     while True:
